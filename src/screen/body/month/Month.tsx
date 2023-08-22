a9e91f7c0defb41bb930bded39b9572542b46e4a
HikosakaTeru <s56529th@fukui.kosen-ac.jp>
2023-08-22 17:39:35 +0900

    月の終わり日に対して閏年を考慮

--- src/screen/body/month/Month.tsx
+++ src/screen/body/month/Month.tsx
@@ -1,24 +1,28 @@
 import Week from "./week/Week"
 
+const dayMax = [31,28,31,30,31,30,31,31,30,31,30,31]
+
 const Month = ({year,month}: {year: number, month: number})=>{
-  const startDayOfWeek = new Date(year,month,1).getDay();  //一時定数
-  console.log(startDayOfWeek);
-  const dayMax = [31,28,31,30,31,30,31,31,30,31,30,31]
-    
-  const rowNum = Math.ceil((dayMax[month] - (7 - startDayOfWeek)) / 7) + 1;  //列の数
-  const loop = new Array(rowNum).fill(0);
+
+  //指定した年月の最終日を取得する（閏年考慮済み）
+  const getLastDay = (year:number, month: number)=>{
+    return (month!==1 || year%4!=0 || (year%100==0 && year%400!=0)) ? dayMax[month] : 29; 
+  }
+
+  const startDayOfWeek = new Date(year,month,1).getDay();  //1日の曜日を取得    
+  const lineNum = Math.ceil((dayMax[month] - (7 - startDayOfWeek)) / 7) + 1;  //列の数
+  const loop = new Array(lineNum).fill(0);
   
-  let startDay:number = -startDayOfWeek-7;
+  let startDay:number = -startDayOfWeek;
 
   return(
     <>
       {
         loop.map(()=>{
-          startDay+=7;
           return(
             <>
               <p className="week" key="week">
-                <Week start={startDay} end={Math.min(dayMax[month],startDay+7)} />
+                <Week start={startDay} end={Math.min(getLastDay(year,month),(startDay+=7))} />
               </p>  
             </>
           )
