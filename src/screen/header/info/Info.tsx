27a43bbe4edc4b41dfdc19a52cd81c66642cf6f4
HikosakaTeru <s56529th@fukui.kosen-ac.jp>
2023-08-22 17:40:54 +0900

    月をボタンで選択できるように変更

--- src/screen/header/info/Info.tsx
+++ src/screen/header/info/Info.tsx
@@ -1,12 +1,36 @@
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Button, Popover,PopoverTrigger,Portal,PopoverContent,PopoverArrow,PopoverHeader,PopoverCloseButton,PopoverBody} from '@chakra-ui/react'
 
 const Info = ({year, month, setTime}: {year:number, month:number, setTime: (year:number,month:number)=>void}) => {
   return (
     <>
       <p className="info" key="info">
-        <Button colorScheme='#d8d8d8' size="xs" color="black" fontSize="xm" onClick={()=>setTime(year,month-1)}>&lt;</Button>
-        <Button colorScheme='#d8d8d8' size="xs" color="black" fontSize="xm">{year}年{month+1}月</Button>
-        <Button colorScheme='#d8d8d8' size="xs" color="black" fontSize="xm" onClick={()=>setTime(year,month+1)}>&gt;</Button>
+        <Button size="xs" color="black" fontSize="xm" onClick={()=>setTime(year,month-1)}>&lt;</Button>
+        <Popover>
+          <PopoverTrigger>
+            <Button size="xs" color="black" fontSize="xm">{year}年{month+1}月</Button>
+          </PopoverTrigger>
+          <Portal>
+            <PopoverContent>
+              <PopoverArrow />
+              <PopoverHeader>年月を変更</PopoverHeader>
+              <PopoverCloseButton />
+              <PopoverBody>
+                <div className='year_choice'>
+                  <Button className="year_button" backgroundColor='#f2e1b1' onClick={()=>setTime(year-1,month)}>{year-1}年</Button>
+                  <Button className="year_button" backgroundColor='#ffdddd' onClick={()=>setTime(new Date().getFullYear(),new Date().getMonth())}>現在</Button>
+                  <Button className="year_button" backgroundColor='#f2e1b1' onClick={()=>setTime(year+1,month)}>{year+1}年</Button>
+                </div>
+                <hr/>
+                <div className='month_set'>
+                  {new Array(12).fill(0).map((_,m)=>{
+                    return(<><Button backgroundColor='#f2e1b1' className="month_button" onClick={()=>setTime(year,m)}>{m+1}月</Button></>)
+                  })}
+                </div>
+              </PopoverBody>
+            </PopoverContent>
+          </Portal>
+        </Popover>      
+        <Button size="xs" color="black" fontSize="xm" onClick={()=>setTime(year,month+1)}>&gt;</Button>
       </p>
     </>
   )
