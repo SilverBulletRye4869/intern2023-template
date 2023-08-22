fc848d00ad295089912e689ef9b7a44f315044fa
HikosakaTeru <s56529th@fukui.kosen-ac.jp>
2023-08-22 17:40:22 +0900

    日付のBoxに曜日を格納

--- src/screen/body/month/week/day/Day.tsx
+++ src/screen/body/month/week/day/Day.tsx
@@ -1,5 +1,5 @@
-const Day = ({date}: { date:number}) => {
-    return(<><p className="day" key="day">{date <= 0 ? "" : date}</p></>)
+const Day = ({date, row}: { date:number, row:number}) => {
+    return(<><p className={`day row${row}`} key="day">{date <= 0 ? "" : date}</p></>)
 }
   
 export default Day;
