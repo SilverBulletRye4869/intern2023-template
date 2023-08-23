1859d73f2c3893d30a1098d1585db641657e30a4
HikosakaTeru <s56529th@fukui.kosen-ac.jp>
2023-08-23 18:02:28 +0900

    閏年でかつ最終日が土曜日の時に、２９日が表示されない不具合の修正

--- src/screen/body/month/Month.tsx
+++ src/screen/body/month/Month.tsx
@@ -1,35 +1,42 @@
-import Week from "./week/Week"
+import Week from "./week/Week";
 
-const dayMax = [31,28,31,30,31,30,31,31,30,31,30,31]
+type Props = {
+  year: number;
+  month: number;
+};
 
-const Month = ({year,month}: {year: number, month: number})=>{
+const dayMax = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
+const Month = ({ year, month }: Props) => {
   //指定した年月の最終日を取得する（閏年考慮済み）
-  const getLastDay = (year:number, month: number)=>{
-    return (month!==1 || year%4!=0 || (year%100==0 && year%400!=0)) ? dayMax[month] : 29; 
-  }
+  const getLastDay = (year: number, month: number) => {
+    return month !== 1 || year % 4 != 0 || (year % 100 == 0 && year % 400 != 0)
+      ? dayMax[month]
+      : 29;
+  };
 
-  const startDayOfWeek = new Date(year,month,1).getDay();  //1日の曜日を取得    
-  const lineNum = Math.ceil((dayMax[month] - (7 - startDayOfWeek)) / 7) + 1;  //列の数
+  const startDayOfWeek = new Date(year, month, 1).getDay(); //1日の曜日を取得
+  const lineNum =
+    Math.ceil((getLastDay(year, month) - (7 - startDayOfWeek)) / 7) + 1; //列の数
   const loop = new Array(lineNum).fill(0);
-  
-  let startDay:number = -startDayOfWeek;
 
-  return(
-    <>
-      {
-        loop.map(()=>{
-          return(
-            <>
-              <p className="week" key="week">
-                <Week start={startDay} end={Math.min(getLastDay(year,month),(startDay+=7))} />
-              </p>  
-            </>
-          )
-       })
-      }
-    </>
-  )
-}
+  let startDay: number = -startDayOfWeek;
+
+  return (
+    <p className="month" key="month">
+      {loop.map((_, i) => {
+        return (
+          <Week
+            year={year}
+            month={month}
+            start={startDay}
+            end={Math.min(getLastDay(year, month), (startDay += 7))}
+            key={`week_${i}`}
+          />
+        );
+      })}
+    </p>
+  );
+};
 
 export default Month;
