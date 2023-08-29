13c989b5a77b8128efc8e12570c07b6d262b37f5
HikosakaTeru <s56529th@fukui.kosen-ac.jp>
2023-08-29 18:01:51 +0900

    ログイン機能の実装

--- src/supabase/Supabase.ts
+++ src/supabase/Supabase.ts
@@ -1,28 +1,32 @@
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
 import type {
   PostgrestSingleResponse,
-  SupabaseClient} from "@supabase/supabase-js";
-import {
-  createClient,
+  Session,
+  SupabaseClient,
 } from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js";
+
+import type { SupabaseResponse } from "~/@types/supabase";
 
 export class Supabase {
-  static TABLE_NAME = "schedule";
-  private SUPABASE: SupabaseClient<any, "public", any>;
+  static TABLE_NAME = "schedules";
+  private SUPABASE: SupabaseClient;
+  private userId = "";
 
   constructor() {
-    this.SUPABASE = createClient(
-      import.meta.env.VITE_PROJECT_URL,
-      import.meta.env.VITE_PROJECT_KEY
-    );
+    const url: string = import.meta.env.VITE_PROJECT_URL as string;
+    const key: string = import.meta.env.VITE_PROJECT_KEY as string;
+    this.SUPABASE = createClient(url, key);
+    void this.getUserId();
   }
 
   public getSchedules = async (
     year: number,
     month: number
-  ): Promise<PostgrestSingleResponse<any[]>> =>
-    await this.SUPABASE.from(Supabase.TABLE_NAME)
+  ): Promise<PostgrestSingleResponse<SupabaseResponse[]>> => {
+    console.log("a id:" + this.userId);
+    return await this.SUPABASE.from(Supabase.TABLE_NAME)
       .select("*")
+      .eq("userId", await this.getUserId())
       .gte("date", `${year}-${`0${month + 1}`.slice(-2)}-01`)
       .lte(
         "date",
@@ -30,6 +34,7 @@ export class Supabase {
           month == 11 ? 0 : month + 2
         }`.slice(-2)}-01`
       );
+  };
 
   public regisisterSchedule = async (
     title: string,
@@ -37,10 +42,11 @@ export class Supabase {
     start: string | null = null,
     end: string | null = null,
     memo: string | null = null
-  ): Promise<PostgrestSingleResponse<any[]>> =>
-    await this.SUPABASE.from(Supabase.TABLE_NAME)
+  ): Promise<PostgrestSingleResponse<SupabaseResponse[]>> => {
+    console.log(this.getUserId());
+    return await this.SUPABASE.from(Supabase.TABLE_NAME)
       .insert({
-        user_id: 0,
+        userId: await this.getUserId(),
         title: title,
         date: `${date[0]}-${`0${date[1]}`.slice(-2)}-${`0${date[2]}`.slice(
           -2
@@ -50,6 +56,7 @@ export class Supabase {
         memo: memo,
       })
       .select();
+  };
 
   public updateSchedule = async (
     uid: number,
@@ -57,7 +64,7 @@ export class Supabase {
     start: string | null = null,
     end: string | null = null,
     memo: string | null = null
-  ) =>
+  ): Promise<PostgrestSingleResponse<SupabaseResponse[]>> =>
     await this.SUPABASE.from(Supabase.TABLE_NAME)
       .update({
         title: title,
@@ -65,12 +72,43 @@ export class Supabase {
         endTime: end ? end + ":00" : null,
         memo: memo,
       })
-      .eq("sche_id", uid);
+      .eq("sche_id", uid)
+      .select();
 
-  public deleteSchedule = async (uid: number) =>
-    await this.SUPABASE.from(Supabase.TABLE_NAME).delete().eq("sche_id", uid);
+  public deleteSchedule = async (
+    uid: number
+  ): Promise<PostgrestSingleResponse<SupabaseResponse[]>> =>
+    await this.SUPABASE.from(Supabase.TABLE_NAME)
+      .delete()
+      .eq("sche_id", uid)
+      .select();
 
-  public get = (): SupabaseClient<any, "public", any> => {
+  public signIn = async () => {
+    this.userId = "";
+    await this.SUPABASE.auth.signInWithOAuth({
+      provider: "discord",
+      options: {
+        queryParams: {
+          access_type: "offline",
+          prompt: "consent",
+        },
+      },
+    });
+  };
+
+  public getUserId = async (): Promise<string | undefined> => {
+    if (this.userId != "") return this.userId;
+    const { data, error } = await this.SUPABASE.auth.getSession();
+    /*
+    console.log(data);
+    console.log(data.session);
+    console.log(data.session?.user);
+    console.log(data.session?.user.id);*/
+    this.userId = data.session?.user.id ?? "";
+    return this.userId;
+  };
+
+  public getClient = (): SupabaseClient => {
     return this.SUPABASE;
   };
 }
